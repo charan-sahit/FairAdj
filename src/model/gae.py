a3c14c484e9e16d6e518dac6b54e170119cec6bf
charan-sahit <charan.sahit@students.iiit.ac.in>
2023-04-30 22:34:42 +0530

    print

--- src/model/gae.py
+++ src/model/gae.py
@@ -59,17 +59,47 @@ class GCNModelAE(nn.Module):
         return [{"params": self.parameters(), "lr_mult": 1.}]
 
 
+# class GCNModelVAE(nn.Module):
+#     def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
+#         super(GCNModelVAE, self).__init__()
+#         self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
+#         self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+#         self.gc3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+#         self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+
+#     def encode(self, x, adj):
+#         hidden1 = self.gc1(x, adj)
+#         return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
+
+#     def reparameterize(self, mu, logvar):
+#         if self.training:
+#             std = torch.exp(logvar)
+#             eps = torch.randn_like(std)
+#             return eps.mul(std).add_(mu)
+#         else:
+#             return mu
+
+#     def forward(self, x, adj):
+#         mu, logvar = self.encode(x, adj)
+#         z = self.reparameterize(mu, logvar)
+#         return self.dc(z), z, mu, logvar
+
+#     def get_parameters(self):
+#         return [{"params": self.parameters(), "lr_mult": 1.}]
+
+from torch_geometric.nn import GATConv
+
 class GCNModelVAE(nn.Module):
     def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
         super(GCNModelVAE, self).__init__()
-        self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
-        self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
-        self.gc3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.gc1 = GATConv(input_feat_dim, hidden_dim1, dropout, heads=8)
+        self.gc2 = GATConv(hidden_dim1*8, hidden_dim2, dropout, heads=1)
+        self.gc3 = GATConv(hidden_dim1*8, hidden_dim2, dropout, heads=1)
         self.dc = InnerProductDecoder(dropout, act=lambda x: x)
 
-    def encode(self, x, adj):
-        hidden1 = self.gc1(x, adj)
-        return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
+    def encode(self, x, edge_index):
+        hidden1 = F.dropout(F.elu(self.gc1(x, edge_index)), p=0.6, training=self.training)
+        return self.gc2(hidden1, edge_index), self.gc3(hidden1, edge_index)
 
     def reparameterize(self, mu, logvar):
         if self.training:
@@ -79,13 +109,14 @@ class GCNModelVAE(nn.Module):
         else:
             return mu
 
-    def forward(self, x, adj):
-        mu, logvar = self.encode(x, adj)
+    def forward(self, x, edge_index):
+        mu, logvar = self.encode(x, edge_index)
         z = self.reparameterize(mu, logvar)
         return self.dc(z), z, mu, logvar
 
     def get_parameters(self):
         return [{"params": self.parameters(), "lr_mult": 1.}]
+
 
 
 class InnerProductDecoder(nn.Module):
